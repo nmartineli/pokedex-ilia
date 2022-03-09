282d0cb068d66a18d6fe9fc8419436500631569f
NataliaMartineli <nataliamartineli@gmail.com>
2022-03-09 07:52:16 -0300

    Created cards, header and search features folders

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import type { NextPage } from 'next'
 
+import styles from '../styles/home.module.scss'
 
 const Home: NextPage = () => {
   return (
-   <h1>Hello world</h1>
+   <h1 className={styles.title}>Hello world</h1>
   )
 }
 
