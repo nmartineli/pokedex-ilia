7fa157c584a348b9e833a16242b66dd559f21a3d
NataliaMartineli <nataliamartineli@gmail.com>
2022-03-10 17:45:30 -0300

    PokeAPI config

--- features/Card/index.tsx
+++ features/Card/index.tsx
@@ -1,15 +1,25 @@
 import Image from 'next/image';
+
 import styles from './styles.module.scss';
 
-export function Card() {
+type Props = {
+	pokemon: {
+		name: string;
+		url: string;
+		pokemonIndex: string;
+		imageUrl: string;
+	};
+};
+
+export function Card({ pokemon }: Props) {
 	return (
 		<section className={styles.cardContainer}>
-			<p>#001</p>
+			<p>#{pokemon.pokemonIndex}</p>
 			<div className={styles.cardImageContainer}>
-				<Image src="/images/placeholder.png" alt="Placeholder" width={72} height={72} className={styles.cardImage} />
+				<Image src={pokemon.imageUrl} alt="Placeholder" width={72} height={72} className={styles.cardImage} />
 			</div>
 			<div className={styles.cardNameContainer}>
-				<h2>Pokémon name</h2>
+				<h2>{pokemon.name}</h2>
 			</div>
 		</section>
 	);
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,74 @@
-import type { NextPage } from 'next';
+import axios from 'axios';
+import type { GetStaticProps } from 'next';
 import Head from 'next/head';
+
 import { Card } from '../features/Card';
 import { Header } from '../features/Header';
 import { Search } from '../features/Search';
 
 import styles from './home.module.scss';
 
-const Home: NextPage = () => {
+type PokemonData = {
+	name: string;
+	url: string;
+};
+
+type Pokemon = {
+	name: string;
+	url: string;
+	pokemonIndex: string;
+	imageUrl: string;
+};
+
+interface PokedexProps {
+	pokemons: Pokemon[];
+}
+
+export default function Home({ pokemons }: PokedexProps) {
 	return (
 		<>
 			<Head>
-				<title>Pokedex</title>
+				<title>Pokédex</title>
 			</Head>
 			<div className={styles.app}>
 				<main>
 					<Header />
 					<Search />
+
 					<div className={styles.cardsContainer}>
-						<Card />
-						<Card />
-						<Card />
-						<Card />
-						<Card />
-						<Card />
-						<Card />
+						{pokemons.map((pokemon, index) => {
+							return <Card pokemon={pokemon} key={index} />;
+						})}
 					</div>
 				</main>
 			</div>
 		</>
 	);
-};
+}
 
-export default Home;
+export const getStaticProps: GetStaticProps = async () => {
+	const apiData = await axios
+		.get('https://pokeapi.co/api/v2/pokemon/?limit=30')
+		.then((res) => res.data.results)
+		.catch((error) => console.error(error));
+
+	const pokemons = apiData.map((pokemonData: PokemonData) => {
+		const pokemonIndex = pokemonData.url.split('/')[6];
+		const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonIndex}.png?raw=true`;
+
+		return {
+			name: pokemonData.name.charAt(0).toUpperCase() + pokemonData.name.slice(1),
+			url: pokemonData.url,
+			pokemonIndex: String(pokemonIndex).padStart(3, '0'),
+			imageUrl,
+		};
+	});
+
+	console.log(pokemons);
+
+	return {
+		props: {
+			pokemons,
+		},
+	};
+};
