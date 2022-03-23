9d991d9bc09b99a664fad4369947fcbcd2a2ae93
NataliaMartineli <nataliamartineli@gmail.com>
2022-03-23 19:12:58 -0300

    API call updated

--- src/features/CardsContainer/index.tsx
+++ src/features/CardsContainer/index.tsx
@@ -4,69 +4,22 @@ import { Card } from '../Card';
 
 import styles from './styles.module.scss';
 
-interface PokemonData {
+type PokemonData = {
 	name: string;
 	url: string;
-}
-
-interface PokemonDataUpdated extends PokemonData {
 	pokemonIndex: string;
 	imageUrl: string;
 	type: string;
+};
+
+interface CardsContainerProps {
+	pokemonData: PokemonData[];
 }
 
-export function CardsContainer() {
-	const [data, setData] = useState<PokemonDataUpdated[]>([]);
-	const [isLoading, setLoading] = useState<boolean>(false);
-
-	async function getApiData() {
-		const apiData = await axios
-			.get('https://pokeapi.co/api/v2/pokemon/?limit=160')
-			.then((res) => res.data.results)
-			.catch((error) => console.error(error));
-
-		return apiData;
-	}
-
-	useEffect(() => {
-		setLoading(true);
-
-		async function loadData() {
-			const apiData = await getApiData();
-
-			const pokemonUpdatedData = await Promise.all(
-				apiData.map(async (pokemonData: PokemonData) => {
-					const pokemonIndex = pokemonData.url.split('/')[6];
-					const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonIndex}.png`;
-
-					const pokemonInformation = await axios
-						.get(`https://pokeapi.co/api/v2/pokemon/${pokemonIndex}/`)
-						.then((res) => res.data)
-						.catch((error) => console.error(error));
-
-					const type = pokemonInformation.types[0].type.name;
-
-					return {
-						name: pokemonData.name.charAt(0).toUpperCase() + pokemonData.name.slice(1),
-						url: pokemonData.url.split('/')[6],
-						pokemonIndex: String(pokemonIndex).padStart(3, '0'),
-						imageUrl,
-						type,
-					};
-				})
-			);
-			setData(pokemonUpdatedData);
-			setLoading(false);
-		}
-		loadData();
-	}, []);
-
-	if (isLoading) return <p>Loading...</p>;
-	if (!data) return <p>No profile data</p>;
-
+export function CardsContainer(props: CardsContainerProps) {
 	return (
 		<div className={styles.cardsContainer}>
-			{data.map((pokemon, index) => {
+			{props.pokemonData.map((pokemon, index) => {
 				return <Card pokemon={pokemon} key={index} />;
 			})}
 		</div>
